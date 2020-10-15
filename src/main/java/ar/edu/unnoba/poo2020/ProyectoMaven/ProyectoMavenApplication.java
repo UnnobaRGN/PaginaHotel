da2b52de05545333eb93e1a91b0d87953b1504ed
German <ger_quiroga@hotmail.com>
2020-10-15 11:04:24 -0300

    Segundo commit

---
+++ src/main/java/ar/edu/unnoba/poo2020/ProyectoMaven/ProyectoMavenApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unnoba.poo2020.ProyectoMaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/unnoba/poo2020/ProyectoMaven/ProyectoMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unnoba.poo2020.ProyectoMaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
