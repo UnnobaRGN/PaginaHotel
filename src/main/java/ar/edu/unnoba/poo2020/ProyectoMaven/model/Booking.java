8ece93c17a6d36114659fa08ef8a437f9fd18471
Ramiro1999 <58752200+Ramiro1999@users.noreply.github.com>
2020-11-04 16:06:34 -0300

    agregar clases

--- src/main/java/ar/edu/unnoba/poo2020/ProyectoMaven/model/Booking.java
+++ src/main/java/ar/edu/unnoba/poo2020/ProyectoMaven/model/Booking.java
@@ -1,4 +1,6 @@
 package ar.edu.unnoba.poo2020.ProyectoMaven.model;
 
 public class Booking {
+
+
 }
--- src/main/java/ar/edu/unnoba/poo2020/ProyectoMaven/model/Room.java
+++ src/main/java/ar/edu/unnoba/poo2020/ProyectoMaven/model/Room.java
@@ -1,4 +1,21 @@
 package ar.edu.unnoba.poo2020.ProyectoMaven.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="Room")
 public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private float price;
+    private int occupancy;
+    private int availability;
+    private String facilities;
+
+
+
 }
