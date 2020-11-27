0e3afbc6172cc321319f1057b6eed5c1bdd85aa5
Ramiro1999 <58752200+Ramiro1999@users.noreply.github.com>
2020-11-26 22:09:10 -0300

    booking y otras cosas

---
+++ src/main/java/ar/edu/unnoba/poo2020/ProyectoMaven/service/RoomServiceImp.java
@@ -0,0 +1,30 @@
+package ar.edu.unnoba.poo2020.ProyectoMaven.service;
+
+import ar.edu.unnoba.poo2020.ProyectoMaven.model.Room;
+import ar.edu.unnoba.poo2020.ProyectoMaven.repository.RoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+@Service
+public class RoomServiceImp implements  IRoomService {
+
+    private RoomRepository roomRepository;
+    @Autowired
+    public RoomServiceImp(RoomRepository roomRepository) {
+        this.roomRepository = roomRepository;
+    }
+
+    @Override
+    public List<Room> getRoomsAvailable(Date checkIn, Date checkOut, int occupancy) {
+        return roomRepository.getRoomsAvailable(checkIn,checkOut,occupancy);
+    }
+
+    @Override
+    public Optional<Room> findby(Long roomId) {
+        return roomRepository.findById(roomId);
+
+    }
+}
